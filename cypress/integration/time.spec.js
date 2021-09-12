b51926b534155ccac21d961fa1cbc2d5af895520
EricaSantos2109 <erica.santos2110@gmail.com>
2021-09-12 19:45:56 -0300

    clock funcionando e erro usando tick

---
+++ cypress/integration/time.spec.js
@@ -0,0 +1,41 @@
+///<reference types="cypress"/>
+
+describe('Work with alerts', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    it('Going back to the past', () => {
+        //cy.get('#buttonNow').click()
+        //cy.get('#resultado > span').should('contain', '12/09/2021')
+
+        //clock reescreve a data ativa relacionada ao tempo para ser controlado de forma sincrona atraves do tick. Reseta o tempo também
+
+        // cy.clock()
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '31/12/1969')
+
+        const dt = new Date(2012, 3, 10, 15, 23, 50) //posso dizer a data que eu quero ano/dia/mes/hora/minutos/segundos
+        cy.clock(dt.getTime())
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '10/04/2012')
+    })
+
+    it.only('Goes to the future', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '15731')
+        cy.get('#resultado > span').invoke('text').should('gt', 157317970)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 5000)
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 15000)
+
+    })
+})
